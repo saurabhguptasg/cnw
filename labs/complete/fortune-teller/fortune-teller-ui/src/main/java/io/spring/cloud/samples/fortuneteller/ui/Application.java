c7891b2d0a8c26c020a0b2dc136e7dc07d76d196
saurabhguptasg <saurabhgupta.sg@gmail.com>
2016-10-06 08:12:41 -0400

    fix rest client

--- labs/complete/fortune-teller/fortune-teller-ui/src/main/java/io/spring/cloud/samples/fortuneteller/ui/Application.java
+++ labs/complete/fortune-teller/fortune-teller-ui/src/main/java/io/spring/cloud/samples/fortuneteller/ui/Application.java
@@ -4,6 +4,8 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.client.circuitbreaker.EnableCircuitBreaker;
 import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+import org.springframework.context.annotation.Bean;
+import org.springframework.web.client.RestTemplate;
 
 @SpringBootApplication
 @EnableCircuitBreaker
@@ -12,5 +14,10 @@ public class Application {
 
     public static void main(String[] args) {
         SpringApplication.run(Application.class, args);
+    }
+
+    @Bean
+    public RestTemplate getRestTemplate() {
+        return new RestTemplate();
     }
 }
